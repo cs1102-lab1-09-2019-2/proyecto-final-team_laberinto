7f22fa61b670577ec0147834f6512b3818386a3d
alexandra-zevallos <54292772+alexandra-zevallos@users.noreply.github.com>
2019-10-24 08:35:43 -0500

    Update laberinto.h

--- laberinto.h
+++ laberinto.h
@@ -1,12 +1,5 @@
-//
-// Created by utec on 24/10/19.
-//
-
-#ifndef TEAM_LABERINTO_LABERINTO_H
-#define TEAM_LABERINTO_LABERINTO_H
-
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
-#endif //TEAM_LABERINTO_LABERINTO_H
+
