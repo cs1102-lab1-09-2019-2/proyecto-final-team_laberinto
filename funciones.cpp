c49ab18f17a80d533a821a2913a3c830f2c9f40c
hillary-github <54276550+hillary-github@users.noreply.github.com>
2019-10-28 06:54:50 -0500

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -1,8 +1,7 @@
 //
 // Created by Gabriel Alexsander on 27/10/2019.
 //
-
-#include "laberinto.h"
+#include "laberinto.cpp"
 
 void CLaberinto::crearLaberinto(){
   Matriz[10][10] = {{1,1,1,1,1,1,1,1,1,1},
