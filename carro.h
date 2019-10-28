537b9c989fa40be7d1cbf0a62bc54a58000a17dc
GABO3Mz <54388029+GABO3Mz@users.noreply.github.com>
2019-10-28 07:35:01 -0500

    Add files via upload

--- carro.h
+++ carro.h
@@ -1,13 +1,26 @@
-//
-// Created by Gabriel Alexsander on 27/10/2019.
-//
-
 #ifndef PROYECTO_FINAL_TEAM_LABERINTO_CARRO_H
 #define PROYECTO_FINAL_TEAM_LABERINTO_CARRO_H
 
-#endif //PROYECTO_FINAL_TEAM_LABERINTO_CARRO_H
 #include <iostream>
-#include <vector>
-#include "funciones.h"
-
+#include <string>
 using namespace std;
+
+class Carro{
+private:
+    string nombre;
+    int Px;
+    int Py;
+public:
+    Carro(string, int, int);
+    ~Carro();
+    void setCarro(string, int, int);
+    string getName();
+    int getPx();
+    int getPy();
+    void MoveR();
+    void MoveL();
+    void MoveU();
+    void MoveD();
+};
+
+#endif //PROYECTO_FINAL_TEAM_LABERINTO_CARRO_H
