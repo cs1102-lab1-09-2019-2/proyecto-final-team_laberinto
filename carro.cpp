7a2524c7ec303f1940fe9a7022db6e518a7c180e
GABO3Mz <54388029+GABO3Mz@users.noreply.github.com>
2019-10-28 07:27:18 -0500

    Add files via upload

--- carro.cpp
+++ carro.cpp
@@ -1,4 +1,41 @@
-//
-// Created by Gabriel Alexsander on 27/10/2019.
-//
+#include "carro.h"
 
+Carro::Carro() {
+}
+
+Carro::~Carro(){
+}
+
+void Carro::setCarro(string _nombre, int _Px, int _Py) {
+    nombre = _nombre;
+    Px = _Px;
+    Py = _Py;
+}
+
+string Carro::getName(){
+    return nombre;
+}
+
+int Carro::getPx(){
+    return Px;
+}
+
+int Carro::getPy(){
+    return Py;
+}
+
+void Carro::MoveR(){
+
+}
+
+void Carro::MoveL(){
+
+}
+
+void Carro::MoveU(){
+
+}
+
+void Carro::MoveD(){
+
+}
