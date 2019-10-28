0e8ff82993d81e9b390135e454d61bb34f92c4ae
hillary-github <54276550+hillary-github@users.noreply.github.com>
2019-10-28 06:53:01 -0500

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -24,3 +24,57 @@ entero CLaberinto::getAltura() {
 entero CLaberinto::getAncho() {
   return ancho;
 }
+void CLaberinto::agregarCarro(CCarro _carro) {
+  entero x, y;void CLaberinto::imprimirLaberinto(){
+  for(entero i = 0; i < altura; i++){
+    for(entero j = 0; j < ancho; j++){
+      cout<< setw(3)<< Matriz[i][j];
+    }
+    cout<< endl;
+  }
+}
+
+
+entero CLaberinto::getAltura() {
+  return altura;
+}
+    
+entero CLaberinto::getAncho() {
+  return ancho;
+}
+  x = _carro.getPosX();
+  y = _carro.getPosY();
+  do{
+    if(Matriz[y][x] == 0) {
+      Matriz[y][x] = 2;
+    }
+    else {
+      _carro.setPosX();
+      _carro.getPosY();
+    }
+  }while(Matriz[y][x] != 2);
+}
+void CLaberinto::imprimirLaberinto(){
+  for(entero i = 0; i < altura; i++){
+    for(entero j = 0; j < ancho; j++){
+      cout<< setw(3)<< Matriz[i][j];
+    }
+    cout<< endl;
+  }
+}
+
+
+
+entero CLaberinto::getAltura() {
+  return altura;
+}
+    
+entero CLaberinto::getAncho() {
+  return ancho;
+}
+
+
+
+
+
+
