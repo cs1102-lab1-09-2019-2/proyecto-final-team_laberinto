2decf7f13e828d0ef825684ae255ca655e2d2afb
alexandra-zevallos <54292772+alexandra-zevallos@users.noreply.github.com>
2019-10-28 05:51:51 -0500

    Update laberinto.h

--- laberinto.h
+++ laberinto.h
@@ -1,5 +1,18 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
+#include "carro.h"
+
+class CLaberinto{
+  private:
+    const entero altura = 10;
+    const entero ancho = 10;
+    entero Matriz[10][10];
+public:
+    CLaberinto();
+    //CLaberinto(entero _altura, entero _ancho):altura(10),ancho(10){};
+    void crearLaberinto();
+    entero getAltura();
+    entero getAncho();
+    void agregarCarro(CCarro _carro);
+    void imprimirLaberinto(); 
+};
 
 
