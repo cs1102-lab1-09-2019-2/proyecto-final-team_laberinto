0941b7581ee31c100613645f3a9dcf9c7046109b
alexandra-zevallos <54292772+alexandra-zevallos@users.noreply.github.com>
2019-10-28 05:53:53 -0500

    Update main_laberinto.cpp

--- main_laberinto.cpp
+++ main_laberinto.cpp
@@ -2,3 +2,19 @@
 // Created by utec on 24/10/19.
 //
 
+#include "laberinto.h"
+
+int main() {
+  CLaberinto laberinto;
+  CCarro carro;
+
+  laberinto.crearLaberinto();
+  laberinto.imprimirLaberinto();
+
+  carro.setPosX();
+  carro.setPosY();
+
+  laberinto.agregarCarro(carro);
+  laberinto.imprimirLaberinto();
+  
+}
