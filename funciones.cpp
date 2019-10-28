1c244c843ac81b4263935cf429dd85d5fb1111e9
alexandra-zevallos <54292772+alexandra-zevallos@users.noreply.github.com>
2019-10-28 05:58:22 -0500

    Avance 1

--- funciones.cpp
+++ funciones.cpp
@@ -16,30 +16,6 @@ void CLaberinto::crearLaberinto(){
                     {1,0,1,1,0,0,0,0,0,1},
                     {1,0,1,0,0,1,1,1,0,1}}
 }
-
-void CLaberinto::agregarCarro(CCarro _carro) {
-  entero x, y;
-  x = _carro.getPosX();
-  y = _carro.getPosY();
-  do{
-    if(Matriz[y][x] == 0) {
-      Matriz[y][x] = 2;
-    }
-    else {
-      _carro.setPosX();
-      _carro.getPosY();
-    }
-  }while(Matriz[y][x] != 2);
-}
-    
-void CLaberinto::imprimirLaberinto(){
-  for(entero i = 0; i < altura; i++){
-    for(entero j = 0; j < ancho; j++){
-      cout<< setw(3)<< Matriz[i][j];
-    }
-    cout<< endl;
-  }
-}
   
 entero CLaberinto::getAltura() {
   return altura;
