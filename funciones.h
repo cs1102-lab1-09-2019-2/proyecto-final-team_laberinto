52c191af78f1c2a000a41f155bd10b288c37cebc
GABO3Mz <54388029+GABO3Mz@users.noreply.github.com>
2019-10-28 07:37:52 -0500

    Add files via upload

--- funciones.h
+++ funciones.h
@@ -1,13 +1,6 @@
-//
-// Created by Gabriel Alexsander on 27/10/2019.
-//
-
 #ifndef PROYECTO_FINAL_TEAM_LABERINTO_FUNCIONES_H
 #define PROYECTO_FINAL_TEAM_LABERINTO_FUNCIONES_H
 
-#endif //PROYECTO_FINAL_TEAM_LABERINTO_FUNCIONES_H
-#include <iostream>
-#include <vector>
-#include "carro.h"
+void PosicionVeri();
 
-using namespace std;
+#endif //PROYECTO_FINAL_TEAM_LABERINTO_FUNCIONES_H
