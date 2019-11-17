03df4614f018bbb1ffd55146ff0e066b35234f7a
alexandra-zevallos <54292772+alexandra-zevallos@users.noreply.github.com>
2019-11-17 15:54:17 -0500

    Funciones actualizada

--- funciones.cpp
+++ funciones.cpp
@@ -70,7 +70,11 @@ entero CLaberinto::getAncho() {
   return ancho;
 }
 
-
+//funciones2.cpp
+void PosicionVeri(int num){
+    while (num < 0 || num > 9){
+        cout << "Posicion invalida, por favor ingrese una nueva: "; cin >> num;
+    }
 
 
 
