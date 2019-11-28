861fb862d73e3496442ad2b25a81bf80b0159110
hillary-github <54276550+hillary-github@users.noreply.github.com>
2019-11-28 07:12:23 -0500

    Create tipos.h

---
+++ tipos.h
@@ -0,0 +1,16 @@
+#ifndef PROYECTO_TIPOS_H
+#define PROYECTO_TIPOS_H
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdio>
+using namespace std;
+
+typedef int TipoEntero;
+typedef double TipoDecimal;
+typedef char TipoCaracter;
+typedef bool TipoTrueFalse;
+typedef string TipoFrase;
+
+#endif //PROYECTO_TIPOS_H
