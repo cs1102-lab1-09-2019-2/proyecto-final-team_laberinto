725acbec1e69b24924d98d8eae3c2910e5011ccb
hillary-github <54276550+hillary-github@users.noreply.github.com>
2019-11-28 12:21:50 -0500

    Add files via upload

---
+++ Tipos.h
@@ -0,0 +1,18 @@
+#ifndef PROYECTO_TIPOS_H
+#define PROYECTO_TIPOS_H
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+typedef int TipoEntero;
+typedef double TipoDecimal;
+typedef char TipoCaracter;
+typedef bool TipoTrueFalse;
+typedef string TipoFrase;
+typedef const char * TipoCaracterConstante;
+
+#endif //PROYECTO_TIPOS_H
